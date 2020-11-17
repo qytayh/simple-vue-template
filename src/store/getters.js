a0f362d8f627c96474fb3f10ada55fb645887e6a
QiXuefen <727968969@qq.com>
2020-11-17 09:34:44 +0800

    windows getter state.user 未定义

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,13 @@
 const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
-  token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name
-}
+  // token: state => state.user.token,
+  // avatar: state => state.user.avatar,
+  // name: state => state.user.name
+  // sidebar: state => '',
+  // device: state => '',
+  token: state => '',
+  avatar: state => '',
+  name: state => ''
+};
 export default getters
